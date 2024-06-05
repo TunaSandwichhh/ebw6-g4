19fdd7dded7190272a14fd52e0b51881ff7b1531
TunaSandwich <davide.faggionato31@gmail.com>
2024-06-05 17:30:22 +0200

    update: updated req mapping

--- energy/src/main/java/it/epicode/energy/controllers/InvoiceController.java
+++ energy/src/main/java/it/epicode/energy/controllers/InvoiceController.java
@@ -15,6 +15,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/invoices")
 public class InvoiceController {
 
   @Autowired
